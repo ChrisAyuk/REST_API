45e835759544b5f2e1386f48c6cd79860f5ffd4d
ChrisAyuk <chrizayuk@gmail.com>
2022-07-22 16:33:23 +0100

    Live Stream control (API list stream request) + (API list by stream id request) (API add stream request)

--- src/main/java/service/rest/api/controller/LiveStreamController.java
+++ src/main/java/service/rest/api/controller/LiveStreamController.java
@@ -1,10 +1,8 @@
 package service.rest.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import service.rest.api.model.LiveStream;
 import service.rest.api.repository.LiveStreamRepo;
 
@@ -27,8 +25,15 @@ public class LiveStreamController {
         return streamRepo.findAll();
     }
 
+    // Get all streams by id from http://localhost:9000/streams{id}
     @GetMapping("/{id}")
     public LiveStream findById(@PathVariable String id){
         return streamRepo.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@RequestBody LiveStream stream) {
+        return streamRepo.create(stream);
     }
 }
